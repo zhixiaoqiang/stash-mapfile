bcedfeb9bd2ecd2c3b51e855f2614e506c9d70e0
纳兹 <nazi@dian.so>
2022-07-27 12:51:38 +0800

    chore: output declaration

---
+++ lib/constants.d.ts
@@ -0,0 +1,11 @@
+/** Cli command enum */
+export declare enum CommandEnum {
+    /** Stash all mapfiles, can be restore */
+    stash = 'stash',
+    /** Remove all mapfiles, can\`t be restore */
+    clean = 'clean',
+    /** Restore all mapfiles */
+    restore = 'restore'
+}
+/** Cli command help info */
+export declare const COMMAND_HELP_INFO = '\nUsage\n  $ stash-mapfile [command] <input>\n\nCommands\n  stash - Stash all mapfiles, can be restore\n  clean - Remove all mapfiles, can`t be restore\n  restore - Restore all mapfiles\n\nOptions\n  --version -v - show version\n  --help -h - show help info\n\nExamples\n  $ stash-mapfile\n  $ stash-mapfile restore\n'
---
+++ lib/utils/index.d.ts
@@ -0,0 +1,2 @@
+/** if env has verbose, then show log */
+export declare const log: (message?: any, ...optionalParams: any[]) => void
